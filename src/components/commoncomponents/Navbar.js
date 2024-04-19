fac445d30c5c244b793cc67572ed0bdb92479947
gautam-36 <gautamkumarg660@gmail.con>
2024-04-19 15:08:26 +0530

    add fav icon

--- src/components/commoncomponents/Navbar.js
+++ src/components/commoncomponents/Navbar.js
@@ -36,7 +36,7 @@ function OffcanvasExample() {
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link href="/">Home</Nav.Link>
                   <Nav.Link href="/about">About</Nav.Link>
-                  <Nav.Link href="/faculty">Notices</Nav.Link>
+                  <Nav.Link href="/notice">Notices</Nav.Link>
                   <Nav.Link href="/faculty">Faculty</Nav.Link>
                   <Nav.Link href="/alumini">Alumini</Nav.Link>
                   <Nav.Link href="/infrastructure">Infrastructure</Nav.Link>
